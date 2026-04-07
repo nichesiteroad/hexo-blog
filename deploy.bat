ae5d252ad7e9a341bb61d06f9400035d448186dc
nichesiteroad <GitHubfreak.men.bin@gmail.com>
2026-04-07 12:13:04 +0800

    update

--- deploy.bat
+++ deploy.bat
@@ -1,15 +1,16 @@
 @echo off
-<<<<<<< HEAD
-cd /d D:\HexoSite
-=======
 cd /d D:\Hexo\hexo-blog
->>>>>>> 05ca0cc (update)
+
 echo ===== Pull latest =====
 git pull --rebase origin main
+
 echo ===== Add =====
 git add .
+
 echo ===== Commit =====
 git commit -m "update"
+
 echo ===== Push =====
 git push origin main
+
 pause
